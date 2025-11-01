3f1f10aa5a3151ba62bb8ac5fcacc965733fa00f
Wingger <tehwingfung@gmail.com>
2025-11-01 15:08:55 +0800

    Changes default page and logout logic

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Models\Item;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 Route::get('/products', function () {
